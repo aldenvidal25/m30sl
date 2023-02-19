e31f27f250f8a2461baae0d0ae6b5889e1af8a0c
Alden Vidal <aldenvidal25@gmail.com>
2023-02-20 05:44:46 +0800

    initial commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            //Admin
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('111'),
+                'phone' => '09468978885',
+                'role' => 'Admin',
+                'status' => 'active',
+
+            ],
+            //User
+            [
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('111'),
+                'phone' => '091256871112',
+                'role' => 'user',
+                'status' => 'active',
+
+            ]
+        ]);
+    }
+}
